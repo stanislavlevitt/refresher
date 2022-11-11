35353ceeda573867ae9ab877afc301ee9456be9f
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-11-11 16:58:54 -0500

    get the backend routes working

--- Postgres Refresher/server.js
+++ Postgres Refresher/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const db = require('./app/models')
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
   console.log("Drop and re-sync db.");
 });
 
@@ -22,6 +22,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', (req,res)=>{
   res.json({message: 'Welcome to the Refresher App'})
 })
+
+require('./app/routes/tutorial.routes')(app)
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, ()=>{
