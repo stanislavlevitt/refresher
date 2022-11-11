122364b8d51d0524e9e5eee976cc2ff88e667b2e
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-11-11 15:29:08 -0500

    skeleton controller

---
+++ Postgres Refresher/app/controllers/tutorial.controller.js
@@ -0,0 +1,11 @@
+const db = require('../models')
+const Tutorial = db.tutorials
+const Op = db.Sequelize.Op
+
+export.create = (req,res)=>{}
+export.findAll = (req,res)=>{}
+export.findOne = (req,res)=>{}
+export.update = (req,res)=>{}
+export.delete = (req,res) =>{}
+export.deleteAll = (req,res)=>{}
+export.findAllPublished = (req,res) =>{}
