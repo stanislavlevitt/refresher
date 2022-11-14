113f896bbff444ab88b3ad16c9d7e9a838f31550
Stanislav Levitt <stanislavlevitt@yahoo.com>
2022-11-14 18:19:50 -0500

    make redux store using toolkit

--- Postgres Refresher/client/src/main.jsx
+++ Postgres Refresher/client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, {createRoot} from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { Provider } from 'react-redux';
+import store from './store'
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
